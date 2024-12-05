9eb679ea4957e56f691f37c867d7b1a1484ed461
dsjramesh7 <rameshdussa07@gmail.com>
2024-12-05 16:56:11 +0530

    created_signin && signup form and page routing

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isToggleSignIn, setIsToggleSignIn] = useState(true);
+
+  const handleToggleSignInForm = () => {
+    setIsToggleSignIn(!isToggleSignIn);
+  };
   return (
-    <div>
+    <>
       <Header />
-      <img
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/2bcf01ee-7ef6-4930-b0d5-c6863853c461/web/IN-en-20241125-TRIFECTA-perspective_a47db038-756f-4f26-b1f7-cfc882b98746_large.jpg"
-        alt="background"
-      />
-    </div>
+      <div className="flex justify-center items-center h-[80vh]">
+        <div className="flex flex-col text-white mb-12 py-12 px-16 bg-black bg-opacity-80">
+          <form action="" className="w-[319px]">
+            <h2 className="font-bold text-4xl mb-7">
+              {isToggleSignIn ? "Sign In" : "Sign Up"}
+            </h2>
+            <div className="flex flex-col gap-8">
+              {!isToggleSignIn && (
+                <input
+                  className="p-4 outline-none bg-gray-700"
+                  type="text"
+                  placeholder="Full Name"
+                />
+              )}
+              <input
+                className="p-4 outline-none bg-gray-700"
+                type="text"
+                placeholder="Email or phone number"
+              />
+              <input
+                className="p-4 outline-none bg-gray-700"
+                type="text"
+                placeholder="password"
+              />
+              <button className="w-full bg-red-500 text-white px-4 py-2 rounded-md">
+                {isToggleSignIn ? "Sign In" : "Sign Up"}
+              </button>
+            </div>
+            <p className="py-4 cursor-pointer" onClick={handleToggleSignInForm}>
+              {isToggleSignIn
+                ? "New to Netflix? Sign Up Now"
+                : "Already Registered? Sign In Now"}
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
